dc7cd5e42e0974387dacb4efdedd2047688a406f
Robert Palmer <robert.palmer@artcom.de>
2010-08-20 17:21:35 +0200

    javascript to fetch the latest two hoccer tweets

--- html_dummy/js/script.js
+++ html_dummy/js/script.js
@@ -1,7 +1,16 @@
 /* Author: 
-
 */
 
+function updateTwitter(json) {
+	$(".tweet p").each( function(index) { 
+		if (!(index < json.length)) {
+		 	return;
+		}
+		
+		var date = new Date(json[index]["created_at"]);
+		$(this).html(json[index]["text"] + " <em>" + date.toLocaleDateString()+ "</em>");
+	});
+}
 
 
 
