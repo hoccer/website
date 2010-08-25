1a024e0559918bfa9fdac3c62ff000091cb458cd
Robert Palmer <robert.palmer@artcom.de>
2010-08-25 12:09:38 +0200

    theming blog and article

--- theme/single.php
+++ theme/single.php
@@ -6,8 +6,22 @@
 
 get_header(); ?>
 
-		<div id="primary">
-			<div id="content">
+        <div class="page">
+          <div class="row">
+            <?php $header_class = (get_post_meta($post->ID, "description", true) ? "" : "page_header_small")   ?>    
+            <div class="column grid_12 page_header <?php echo $header_class ?>">
+              <h1>Blog</h1>
+              <p class="grid_5">
+                <?php 
+                  echo get_post_meta($post->ID, "description", true) ?>
+              </p>
+            </div>
+          </div>
+
+	      <div class="row" id="articles">
+	          <div class="column grid_7">
+              
+    
 
 			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
@@ -16,14 +30,17 @@ get_header(); ?>
 					<div class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'themename' ) . '</span> %title' ); ?></div>
 					<div class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'themename' ) . '</span>' ); ?></div>
 				</nav><!-- #nav-above -->
-
+                
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 					<header class="entry-header">
-						<h1 class="entry-title"><?php the_title(); ?></h1>
+						<h2 class="entry-title"><?php the_title(); ?></h1>
 
 						<div class="entry-meta">
 							<?php
-								printf( __( '<span class="meta-prep meta-prep-author">Posted on </span><a href="%1$s" rel="bookmark"><time class="entry-date" datetime="%2$s" pubdate>%3$s</time></a> <span class="meta-sep"> by </span> <span class="author vcard"><a class="url fn n" href="%4$s" title="%5$s">%6$s</a></span>', 'themename' ),
+								printf( __( '<span class="meta-prep meta-prep-author">Posted on </span>
+								<a href="%1$s" rel="bookmark"><time class="entry-date" datetime="%2$s" pubdate>%3$s</time></a> 
+								<span class="meta-sep"> by </span> <span class="author vcard">
+								<a class="url fn n" href="%4$s" title="%5$s">%6$s</a></span>', 'themename' ),
 									get_permalink(),
 									get_the_date( 'c' ),
 									get_the_date(),
@@ -32,12 +49,14 @@ get_header(); ?>
 									get_the_author()
 								);
 							?>
+						    
 						</div><!-- .entry-meta -->
 					</header><!-- .entry-header -->
 
 					<div class="entry-content">
 						<?php the_content(); ?>
-						<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'themename' ), 'after' => '</div>' ) ); ?>
+						<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'themename' ), 'after' => '</div>' ) ); ?>    
+						
 					</div><!-- .entry-content -->
 
 					<footer class="entry-meta">
@@ -60,19 +79,21 @@ get_header(); ?>
 						<?php edit_post_link( __( 'Edit', 'themename' ), '<span class="edit-link">', '</span>' ); ?>
 					</footer><!-- .entry-meta -->
 				</article><!-- #post-<?php the_ID(); ?> -->
-
+                
 				<nav id="nav-below">
 					<h1 class="screen-reader-text"><?php _e( 'Post navigation', 'themename' ); ?></h1>
 					<div class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'themename' ) . '</span> %title' ); ?></div>
 					<div class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'themename' ) . '</span>' ); ?></div>
 				</nav><!-- #nav-below -->
-
+                
 				<?php comments_template( '', true ); ?>
 
 			<?php endwhile; // end of the loop. ?>
-
+            </div>
+            <div class="column grid_1">&nbsp;</div>
+             <div class="column grid_4">
+               <?php get_sidebar(); ?>
+             </div>
 			</div><!-- #content -->
 		</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
