f1061b0caf13649ad5d2e6b69b59afa445319311
hukl <contact@smyck.org>
2010-08-25 17:42:02 +0200

    archives page beautified

--- theme/archive.php
+++ theme/archive.php
@@ -5,30 +5,43 @@
  */
 
 get_header(); ?>
+<div class="page">
+  <div class="row">
+    <?php $header_class = (get_post_meta($post->ID, "description", true) ? "" : "page_header_small")   ?>
+    <div class="column grid_12 page_header <?php echo $header_class ?>">
+      <h1>Archives</h1>
+      <p class="grid_5">
+        <?php
+          echo get_post_meta($post->ID, "description", true) ?>
+      </p>
+    </div>
+  </div>
+  <div class="row" id="articles">
+    <div class="column grid_7">
+      <?php the_post(); ?>
 
-		<div id="primary">
-			<div id="content">
+      <h2 class="page-title">
+      <?php if ( is_day() ) : ?>
+        <?php printf( __( 'Daily Archives: <span>%s</span>', 'themename' ), get_the_date() ); ?>
+      <?php elseif ( is_month() ) : ?>
+        <?php printf( __( 'Monthly Archives: <span>%s</span>', 'themename' ), get_the_date( 'F Y' ) ); ?>
+      <?php elseif ( is_year() ) : ?>
+        <?php printf( __( 'Yearly Archives: <span>%s</span>', 'themename' ), get_the_date( 'Y' ) ); ?>
+      <?php else : ?>
+        <?php _e( 'Blog Archives', 'themename' ); ?>
+      <?php endif; ?>
+      </h2>
 
-				<?php the_post(); ?>
+      <?php rewind_posts(); ?>
 
-				<h2 class="page-title">
-				<?php if ( is_day() ) : ?>
-					<?php printf( __( 'Daily Archives: <span>%s</span>', 'themename' ), get_the_date() ); ?>
-				<?php elseif ( is_month() ) : ?>
-					<?php printf( __( 'Monthly Archives: <span>%s</span>', 'themename' ), get_the_date( 'F Y' ) ); ?>
-				<?php elseif ( is_year() ) : ?>
-					<?php printf( __( 'Yearly Archives: <span>%s</span>', 'themename' ), get_the_date( 'Y' ) ); ?>
-				<?php else : ?>
-					<?php _e( 'Blog Archives', 'themename' ); ?>
-				<?php endif; ?>
-				</h2>
+      <?php get_template_part( 'loop', 'archive' ); ?>
 
-				<?php rewind_posts(); ?>
+    </div><!-- #primary -->
+    <div class="column grid_1">&nbsp;</div>
+    <div class="column grid_4">
+      <?php get_sidebar(); ?>
+    </div>
+  </div>
 
-				<?php get_template_part( 'loop', 'archive' ); ?>
-
-			</div><!-- #content -->
-		</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
+</div>
 <?php get_footer(); ?>
