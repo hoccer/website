b3e6a826ff69b9729c128d3c8f33823c53180f4a
Robert Palmer <robert.palmer@artcom.de>
2010-08-27 10:33:09 +0200

    removed piwik from footer, using plugin

--- theme/footer.php
+++ theme/footer.php
@@ -70,18 +70,6 @@
 <!--[if lt IE 7 ]>
   <script src="js/dd_belatedpng.js?v=1"></script>
 <![endif]-->
-<!-- Piwik -->
-<script type="text/javascript">
-var pkBaseURL = (("https:" == document.location.protocol) ? "https://stats.artcom.de/" : "http://stats.artcom.de/");
-document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
-</script><script type="text/javascript">
-try {
-var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 1);
-piwikTracker.trackPageView();
-piwikTracker.enableLinkTracking();
-} catch( err ) {}
-</script><noscript><p><img src="http://stats.artcom.de/piwik.php?idsite=1" style="border:0" alt=""/></p></noscript>
-<!-- End Piwik Tag -->
 <?php wp_footer() ?>
 </body>
 </html>
