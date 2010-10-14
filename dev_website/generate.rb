5eea8fe7b5d5539c76dc3a2613d78c2367de0484
Robert Palmer <robert.palmer@artcom.de>
2010-10-14 16:01:48 +0200

    replacing links

--- dev_website/generate.rb
+++ dev_website/generate.rb
@@ -6,20 +6,55 @@ require 'tempfile'
 require 'gollum'
 require 'erb'
 
+@links = {
+  "Developer-website" => "index",
+  "More-about-the-hoccer-api" => "about_api",
+  "Hoccer-Server-Implementation-V3" => "api_overview"
+}
+
 def erb file
   file = File.read("views/#{file}.erb")
   template = ERB.new file
   template.result(binding)
 end
 
+def generate_html wiki_name, page_name, template
+  wiki = Gollum::Wiki.new(wiki_name)
+  page = wiki.page(page_name)
+  
+  @html = replace_links page.formatted_data
+  
+  output_file = @links[page_name]  
+  File.open("html/#{output_file}.html", "w+") do |file|
+    file.write erb template
+  end 
+end
+
+def replace_links website 
+  doc = Nokogiri::HTML(%{<div id="gollum-root">} + website + %{</div>})
+  
+  doc.css("div#gollum-root a").each do |link| 
+    old_href = link.attribute("href").value.slice(1..-1)
+
+    if @links[old_href]
+      new_href = "#{@links[old_href]}.html"
+      link.set_attribute("href", new_href) 
+    end
+  end
+  
+  doc.css("div#gollum-root")
+end
 
 def main
-  wiki = Gollum::Wiki.new("overview")
-  page = wiki.page("Developer-website")
+  output_dir = "html"
   
-  @html = page.formatted_data
+  FileUtils.rm_r(output_dir) if File.exists?(output_dir)
+  FileUtils.mkdir(output_dir)
   
-  puts erb :index
+  generate_html "overview", "Developer-website", :index
+  generate_html "overview", "More-about-the-hoccer-api", :index
+  generate_html "server", "Hoccer-Server-Implementation-V3", :documantation
+  
 rescue => e
   puts "oops: #{e}\n#{e.backtrace.join "\n"}"
   exit 1
