7af42f50f66fe585544d6bccf1992d7f550200c8
Robert Palmer <robert.palmer@artcom.de>
2010-10-14 11:39:20 +0200

    using erb templates for creating html

--- dev_website/generate.rb
+++ dev_website/generate.rb
@@ -4,12 +4,22 @@ require 'rubygems'
 require 'fileutils'
 require 'tempfile'
 require 'gollum'
+require 'erb'
+
+def erb file
+  file = File.read("views/#{file}.erb")
+  template = ERB.new file
+  template.result(binding)
+end
+
 
 def main
   wiki = Gollum::Wiki.new("overview")
   page = wiki.page("Developer-website")
   
-  puts page.formatted_data
+  @html = page.formatted_data
+  
+  puts erb :index
 rescue => e
   puts "oops: #{e}\n#{e.backtrace.join "\n"}"
   exit 1
