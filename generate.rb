13039ce15c2791a9c7940e256612bbc4af16a528
Rodja Trappe <rodja@hoccer.com>
2010-10-22 09:52:20 +0200

    submodule tricks and no commits when callling ./generate.rb

--- generate.rb
+++ generate.rb
@@ -3,13 +3,15 @@
 @root_dir = File.expand_path(File.dirname(__FILE__))
 
 def update_module name
+  puts "updateing module #{name}"
   dir = File.join(@root_dir, name)
-  system("cd #{dir} && git pull")
+  system("cd #{dir} && git reset --hard && git pull")
   system("cd #{@root_dir} && git add #{name}")
 end
 
 def refresh_main_repo
-  system("cd #{@root_dir} && git commit --allow-empty -m 'refreshed content from wikis'")
+  puts "refreshing main repo"
+  #system("cd #{@root_dir} && git commit --allow-empty -m 'refreshed content from wikis'")
   system("cd #{@root_dir} && git submodule update")
 end
 
@@ -18,7 +20,7 @@ def main
   update_module "developer/wikis/overview"
   
   system("cd #{@root_dir} && ./generate_html.rb")
-  system("cd #{@root_dir} && git add developer/public")
+  #system("cd #{@root_dir} && git add developer/public")
   
   refresh_main_repo
 rescue => e 
