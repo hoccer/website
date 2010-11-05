30e9d7fe8de3d3de70a3f2b0d1be1a075e409278
hukl <contact@smyck.org>
2010-11-05 17:19:01 +0100

    layout refactoring and seperation of signup into extra view

--- developer/public/javascripts/api-keys.js
+++ developer/public/javascripts/api-keys.js
@@ -5,32 +5,32 @@ function goToByScroll(id){
 
 $(document).ready(function(){
 
-  $('#login section p a').bind("click", function(){
-    $('#login').hide();
-    $('#signup').show();
-    return false;
-  });
+  //$('#login section p a').bind("click", function(){
+  //  $('#login').hide();
+  //  $('#signup').show();
+  //  return false;
+  //});
 
-  $('#signup section p a').bind("click", function(){
-    $('#signup').hide();
-    $('#login').show();
-    return false;
-  });
+  //$('#signup section p a').bind("click", function(){
+  //  $('#signup').hide();
+  //  $('#login').show();
+  //  return false;
+  //});
 
   $('#signin_errors').hide();
   if ($('#signin_errors').text().length > 0) {
     $('#signin_errors').delay(300).slideDown();
   }
 
-  $('.signup_link').each( function() {
-    $(this).bind("click", function() {
-      $('#login').hide();
-      $('#signup').show();
-      goToByScroll('account_box');
-      return false;
-    });
-  });
-
+  //$('.signup_link').each( function() {
+  //  $(this).bind("click", function() {
+  //    $('#login').hide();
+  //    $('#signup').show();
+  //    goToByScroll('account_box');
+  //    return false;
+  //  });
+  //});
+  alert("foo");
   $("body").append('<script type="text/javascript" src="http://api.twitter.com/1/statuses/user_timeline/hoccer.json?include_rts=true&count=2&callback=updateTwitter"></script');
 });
 
