1eeb76c334f514a9064b129cf6889cde18cc8d9f
Robert Palmer <hi@robertpalmer.de>
2011-03-15 12:18:25 +0100

    scoping stats by api keys

--- developer/app/controllers/statistics_controller.rb
+++ developer/app/controllers/statistics_controller.rb
@@ -1,8 +1,9 @@
 class StatisticsController < ApplicationController
-
+  before_filter :authenticate_account!
+  
   def per_date
     respond_to do |format|
-      format.js   { render :json => ApiStats.unique_users.to_json }
+      format.js   { render :json => ApiStats.unique_users_for_api_key(current_account.api_key).to_json }
     end
   end
 end
