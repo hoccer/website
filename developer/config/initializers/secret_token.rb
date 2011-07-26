b65f8261e8a4e27dfd11a40b56d1876629e4d7f0
Wolke <wolke@hoccer.com>
2011-07-26 11:44:50 +0200

    state of the fucking art on a production server. Y U NO COMMIT UR CHANGES?

--- developer/config/initializers/secret_token.rb
+++ developer/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-NewApi::Application.config.secret_token = 'eda2b7da2d0e33c5ecc2eee5beba60a5faa25b2e4710dc7f682fa79ed6b4e5eccb5f5440d7cde145bf52b807d36e0bd398dba1e15e5817d4a40fb9b13871adae'
+NewApi::Application.config.secret_token = 'eda2b7da9d0e33c5ecc2eee5beba60a5faa25b2e4710dc7f682fa79ed6b4e5eccb5f5440d7cde145bf52b807d36e0bd398dba1e15e5817d4a40fb9b13871adae'
